a8b0ceb4bf4adc3b151693a107f3a6f45a357816
Aaron Larner <anlarner@gmail.com>
2015-06-15 21:28:10 -0500

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,65 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+	// The name `constructor` is important here
+	constructor: function () {
+		// Calling the super constructor is important so our generator is correctly set up
+		generators.Base.apply(this, arguments);
+	},
+	dependencies: function() {
+		this.npmInstall([
+			'gulp',
+			'watchify',
+			'browserify',
+			'vinyl-source-stream',
+			'vinyl-buffer',
+			'gulp-util',
+			'gulp-sourcemaps',
+			'lodash.assign',
+			'babelify'
+		], { 'saveDev': true });
+		this.npmInstall([
+			'react',
+			'jquery',
+			'backbone'
+		], { 'save': true });
+	},
+	indexfile: function() {
+		this.fs.copyTpl(
+			this.templatePath('index.html'),
+			this.destinationPath('./index.html'),
+			{}
+		);
+	},
+	gulpfile: function() {
+		this.fs.copyTpl(
+			this.templatePath('gulpfile.js'),
+			this.destinationPath('./gulpfile.js'),
+			{}
+		);
+	},
+	mainfile: function() {
+		this.fs.copyTpl(
+			this.templatePath('scripts/main.js'),
+			this.destinationPath('./scripts/main.js'),
+			{}
+		);
+	},
+	directories: function() {
+		this.fs.copyTpl(
+			this.templatePath('.gitkeep'),
+			this.destinationPath('./scripts/models'),
+			{}
+		);
+		this.fs.copyTpl(
+			this.templatePath('.gitkeep'),
+			this.destinationPath('./scripts/collections'),
+			{}
+		);
+		this.fs.copyTpl(
+			this.templatePath('.gitkeep'),
+			this.destinationPath('./scripts/components'),
+			{}
+		);
+	}
+});
